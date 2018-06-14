11aa69b0fb282e99284c4bc804027dc8ccb9bca9
benjaminboitrelle <benjamin.boitrelle@desy.de>
2018-06-14 12:00:43 +0200

    Modify way of calling data to fit (WIP)

--- calibration/src/process/adccal/methods/process_adccal_default.py
+++ calibration/src/process/adccal/methods/process_adccal_default.py
@@ -26,65 +26,43 @@ class Process(ProcessAdccalBase):
                 "data": np.zeros(shapes["offset"]),
                 "path": "sample/coarse/slope"
             },
-            "s_fine_offset": {
-                "data": np.zeros(shapes["offset"]),
-                "path": "sample/fine/offset"
-            },
-            "s_fine_slope": {
-                "data": np.zeros(shapes["offset"]),
-                "path": "sample/fine/slope"
-            },
         }
-        self._coarse_fitting_range = self._method_properties["coarse_fitting_range"]
-        self._fine_fitting_range = self._method_properties["fine_fitting_range"]
 
     def _calculate(self):
-        ''' Perform a linear fit on sample ADC coarse/fine and store the offset
+        ''' Perform a linear fit on sample ADC coarse and store the offset
             and slope in a HDF5 file.
         '''
         print("Start loading data from {} ...".format(self._in_fname), end="")
         data = self._load_data(self._in_fname)
-        print("Data loaded, fitting coarse and data...")
+        print("Data loaded, fitting coarse data...")
+
+        data_type = "s_coarse"
+        offset_type = "s_coarse_offset"
+        slope_type = "s_coarse_slope"
 
         # convert (n_adcs, n_cols, n_groups, n_frames)
         #      -> (n_adcs, n_cols, n_groups * n_frames)
-        self._merge_groups_with_frames(data["s_coarse"])
-        self._merge_groups_with_frames(data["s_fine"])
+        self._merge_groups_with_frames(data[data_type])
 
         # create as many entries for each vin as there were original frames
         vin = self._fill_up_vin(data["vin"])
-        sample_coarse = data["s_coarse"]
-        offset_coarse = self._result["s_coarse_offset"]["data"]
-        slope_coarse = self._result["s_coarse_slope"]["data"]
-        sample_fine = data["s_fine"]
-        offset_fine = self._result["s_fine_offset"]["data"]
-        slope_fine = self._result["s_fine_slope"]["data"]
+        sample_coarse = data[data_type]
+        offset = self._result[offset_type]["data"]
+        slope = self._result[slope_type]["data"]
 
         for adc in range(self._n_adcs):
             for col in range(self._n_cols):
                 adu_coarse = sample_coarse[adc, col, :]
-                adu_fine = sample_fine[adc, col, :]
-                #print(len(vin), len(adu_fine), len(adu_coarse))
-                idx = np.where(np.logical_and(adu_coarse < self._coarse_fitting_range[1],
-                                              adu_coarse > self._coarse_fitting_range[0]))
-                idx_fine = np.where(np.logical_and(adu_fine < self._fine_fitting_range[1],
-                                                   adu_fine > self._fine_fitting_range[0]))
+                idx = np.where(np.logical_and(adu_coarse < 30,
+                                              adu_coarse > 1))
                 if np.any(idx):
                     fit_result = self._fit_linear(vin[idx], adu_coarse[idx])
-                    slope_coarse[adc, col], offset_coarse[adc, col] = fit_result.solution
+                    slope[adc, col], offset[adc, col] = fit_result.solution
                 else:
-                    slope_coarse[adc, col] = np.NaN
-                    offset_coarse[adc, col] = np.NaN
-                if np.any(idx_fine):
-                    fit_result_fine = self._fit_linear(vin[idx_fine], adu_fine[idx_fine])
-                    slope_fine[adc, col], offset_fine[adc, col] = fit_result_fine.solution
-                else:
-                    slope_fine[adc, col] = np.NaN
-                    offset_fine[adc, col] = np.NaN
+                    slope[adc, col] = np.NaN
+                    offset[adc, col] = np.NaN
 
-        self._result["s_coarse_slope"]["data"] = slope_coarse
-        self._result["s_coarse_offset"]["data"] = offset_coarse
-        self._result["s_fine_slope"]["data"] = slope_fine
-        self._result["s_fine_offset"]["data"] = offset_fine
+        self._result[slope_type]["data"] = slope
+        self._result[offset_type]["data"] = offset
 
-        print("Fitting is done.")
+        print("Coarse fitting is done.")
