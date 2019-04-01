aee0f91a1dbee317069275776d9db9a07cafda22
benjaminboitrelle <benjamin.boitrelle@desy.de>
2019-04-01 15:03:19 +0200

    Remove useless comments and add reset parameters

--- calibration/src/correction/adccal/methods/correction_adccal_default.py
+++ calibration/src/correction/adccal/methods/correction_adccal_default.py
@@ -38,7 +38,12 @@ class Correction(CorrectionAdccalBase):
         }
 
     def _adc_reordering(self, adc_to_reorder):
-
+        ''' Reshuffle adc arrays
+                Input data (4D arrays):
+                    (n_adcs, n_cols, n_frames, n_groups)
+                Output data (3D arrays):
+                    (n_rows, n_cols, n_frames)
+        '''
         adc_shaped = np.zeros((self._n_rows, self._n_cols, self._n_frames))
         for grp in range(self._n_groups):
             for adc in range(self._n_adcs):
@@ -57,19 +62,6 @@ class Correction(CorrectionAdccalBase):
 
         return (adc_ramp - offset) / slope * adu_max
 
-
-#    def apply_correction(self, coarse, fine, offset_coarse, slope_coarse, offset_fine, slope_fine):
-#        ADU_MAX = 4095
-#
-#        for frame in range(self._n_frames):
-#            crs = coarse[:, :, frame, :]
-#            coarse_cor = (crs - offset_coarse) / slope_coarse * (- ADU_MAX/2)
-#            fine = fine[:, :, frame, :]
-#            fine_cor = (fine - offset_fine) / slope_fine * ADU_MAX
-#            adc_corrected[:, :, frame] = coarse_cor - fine_cor + ADU_MAX
-#
-#        return adc_corrected
-
     def _calculate(self):
         ''' Read gathered data, processed coarse and fine data to apply
             a correction.
@@ -85,45 +77,39 @@ class Correction(CorrectionAdccalBase):
 
         sample_crs = data_gathered["s_coarse"]
         reset_crs = data_gathered["r_coarse"]
-        offset_crs = data_processed["s_coarse_offset"]
-        slope_crs = data_processed["s_coarse_slope"]
+        s_offset_crs = data_processed["s_coarse_offset"]
+        r_offset_crs = data_processed["r_coarse_offset"]
+        s_slope_crs = data_processed["s_coarse_slope"]
+        r_slope_crs = data_processed["r_coarse_slope"]
         sample_fn = data_gathered["s_fine"]
         reset_fn = data_gathered["r_fine"]
-        offset_fn = data_processed["s_fine_offset"]
-        slope_fn = data_processed["s_fine_slope"]
+        s_offset_fn = data_processed["s_fine_offset"]
+        r_offset_fn = data_processed["r_fine_offset"]
+        s_slope_fn = data_processed["s_fine_slope"]
+        r_slope_fn = data_processed["r_fine_slope"]
         sample_corrected = self._result["s_adc_corrected"]["data"]
         reset_corrected = self._result["r_adc_corrected"]["data"]
-        self._result["n_frames_per_run"]["data"] = data_gathered["n_frames_per_run"]
+        n_frames_per_run = data_gathered["n_frames_per_run"]
+        self._result["n_frames_per_run"]["data"] = n_frames_per_run
         ADU_MAX = 4095
-
-#        off_crs = np.reshape(offset_crs, (7, 32, 1, 212))
-#        slp_crs = np.reshape(slope_crs, (7, 32, 1, 212))
-#        off_fn = np.reshape(offset_fn, (7, 32, 1, 212))
-#        slp_fn = np.reshape(slope_fn, (7, 32, 1, 212))
-#        s_crs_cor = (sample_crs - off_crs) / slp_crs * (-ADU_MAX/2) + ADU_MAX
-#        s_fn_cor = (sample_fn - off_fn) / slp_fn * ADU_MAX
-#        r_crs_cor = (reset_crs - off_crs) / slp_crs * (-ADU_MAX/2) + ADU_MAX
-#        r_fn_cor = (reset_fn - off_fn) / slp_fn * ADU_MAX
-#        sample_corrected = s_crs_cor - s_fn_cor
-#        reset_corrected = r_crs_cor - r_fn_cor
 
         for frame in range(self._n_frames):
             np.seterr(divide='ignore', invalid='ignore')
             s_crs_cor = self.correction_crs_fn(sample_crs[:, :, frame, :],
-                                               offset_crs,
-                                               slope_crs,
+                                               s_offset_crs,
+                                               s_slope_crs,
                                                -ADU_MAX/2)
             s_fn_cor = self.correction_crs_fn(sample_fn[:, :, frame, :],
-                                              offset_fn,
-                                              slope_fn,
+                                              s_offset_fn,
+                                              s_slope_fn,
                                               ADU_MAX)
             r_crs_cor = self.correction_crs_fn(reset_crs[:, :, frame, :],
-                                               offset_crs,
-                                               slope_crs,
+                                               r_offset_crs,
+                                               r_slope_crs,
                                                -ADU_MAX/2)
             r_fn_cor = self.correction_crs_fn(reset_fn[:, :, frame, :],
-                                              offset_fn,
-                                              slope_fn,
+                                              r_offset_fn,
+                                              r_slope_fn,
                                               ADU_MAX)
             sample_corrected[:, :, frame, :] = s_crs_cor - s_fn_cor + ADU_MAX
             reset_corrected[:, :, frame, :] = r_crs_cor - r_fn_cor + ADU_MAX
