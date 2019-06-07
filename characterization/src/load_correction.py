151f50f9c5fa3177021e535d41df1a3c7158d4b1
benjaminboitrelle <benjamin.boitrelle@desy.de>
2019-06-07 14:10:08 +0200

    Remove useless comments

--- characterization/src/load_correction.py
+++ characterization/src/load_correction.py
@@ -27,7 +27,6 @@ class LoadCorrection():
             }
 
         self._metadata_paths = {
-#            "vin": "vin",
             "n_frames_per_run": "collection/n_frames_per_run"
         }
 
@@ -45,7 +44,6 @@ class LoadCorrection():
 
     def set_input_fname(self):
         self._input_fname = self._get_input_fname(self._input_fname_templ)
-
 
     def get_dimensions_data(self):
         data = self.load_data_all()
@@ -67,38 +65,6 @@ class LoadCorrection():
         file = glob.glob(input_fname)
 
         return file[0]
-#        input_fname = input_fname_templ.format(data_type=self._data_type,
-#                                               col_start="*",
-#                                               col_stop="*")
-#
-#        files = glob.glob(input_fname)
-#
-#        # TODO do not use file name but "collections/columns_used" entry in
-#        # files
-#        prefix, middle = input_fname_templ.split("{col_start}")
-#        middle, suffix = middle.split("{col_stop}")
-#
-#        prefix = prefix.format(data_type=self._data_type)
-#        middle = middle.format(data_type=self._data_type)
-#        suffix = suffix.format(data_type=self._data_type)
-#
-#        searched_file = None
-#        for f in files:
-#            searched_file = f
-#            cols = f[len(prefix):-len(suffix)]
-#            cols = map(int, cols.split(middle))
-#            # convert str into int
-#            cols = list(map(int, cols))
-#
-#            if cols[0] <= col and col <= cols[1]:
-#                searched_file = f
-#                col_offset = cols[0]
-#                break
-#        if searched_file is None:
-#            raise Exception("No files found which contains column {}."
-#                            .format(col))
-#
-#        return searched_file, col_offset
 
     def load_data(self):
 
@@ -114,20 +80,3 @@ class LoadCorrection():
                                                 self._row,
                                                 self._col]
         return data
-
-#    def load_data(self):
-#        col = self._col - self._col_offset
-#
-#        data = {}
-#        with h5py.File(self._input_fname, "r") as f:
-#            vin = f[self._metadata_paths["vin"]][()]
-#
-#            n_frames_per_run = self._metadata_paths["n_frames_per_run"]
-#            self._n_frames_per_vin = f[n_frames_per_run][()]
-#
-#            for key in self._paths:
-#                data[key] = {}
-#                for subkey, path in self._paths[key].items():
-#                    data[key][subkey] = f[path][(self._row, col, slice(None))]
-#
-#        return vin, data
