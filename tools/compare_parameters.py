890348f17153fda65fcbb3c3e588515110655578
benjaminboitrelle <benjamin.boitrelle@desy.de>
2019-11-22 14:09:32 +0100

    Define initialisation of class

--- tools/compare_parameters.py
+++ tools/compare_parameters.py
@@ -12,8 +12,30 @@ class CompareParameters(object):
     """This class takes two arrays and compare them.
     """
 
-    def __init__(self, input_dir, out_fname):
-        pass
+    def __init__(self, input_dir_1, input_dir_2, out_fname):
+        self._input_fname = None
+        self._input_dir_1 = input_dir_1
+        self._input_dir_2 = input_dir_2
+        self._output_fname = out_fname
+
+        self._paths_constants = {
+            "s_coarse": {
+                "slope": "sample/coarse/slope",
+                "offset": "sample/coarse/offset"
+            },
+            "r_coarse": {
+                "slope": "reset/coarse/slope",
+                "offset": "reset/coarse/offset"
+            },
+            "s_fine": {
+                "slope": "sample/fine/slope",
+                "offset": "sample/fine/offset"
+            },
+            "r_fine": {
+                "slope": "reset/fine/slope",
+                "offset": "reset/fine/offset"
+            }
+        }
 
     def load_data(self):
         pass
